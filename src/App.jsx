e14ed4beb430def3bcc8d9623adeebdc09972c52
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-11 21:47:13 +0530

    Desktop and Mobile Nav Initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 function App() {
   return (
   <div>
+    {/* Navbar */}
     <div className="flex items-center justify-between bg-slate-900 text-white px-4">
     <div className="font-bold">Logo</div>
+    {/* Desktop Nav */}
+    <div className="hidden sm:flex gap-2">
+      <span>Home</span>
+      <span>About</span>
+      <span>Contact</span>
+    </div>
+    <button>☰</button>
+    {/* Mobile Nav */}
     <div className="flex gap-2">
       <span>Home</span>
       <span>About</span>
@@ -10,12 +19,12 @@ function App() {
     </div>
   </div>
   <div className="grid sm:grid-cols-2 md:grid-cols-3 text-white p-6 gap-6 text-center font-semibold sm:text-sm ">
-    <div className="bg-slate-500 p-4 rounded">Feature1</div>
-    <div className="bg-slate-500 p-4 rounded">Feature2</div>
-    <div className="bg-slate-500 p-4 rounded">Feature3</div>
-    <div className="bg-slate-500 p-4 rounded">Feature4</div>
-    <div className="bg-slate-500 p-4 rounded">Feature5</div>
-    <div className="bg-slate-500 p-4 rounded">Feature6</div>
+    <div className="bg-slate-500 p-4 rounded hover:bg-slate-600 hover:scale-105 transition-all duration-500">Feature1</div>
+    <div className="bg-slate-500 p-4 rounded hover:bg-slate-600 hover:scale-105">Feature2</div>
+    <div className="bg-slate-500 p-4 rounded hover:bg-slate-600">Feature3</div>
+    <div className="bg-slate-500 p-4 rounded hover:bg-slate-600">Feature4</div>
+    <div className="bg-slate-500 p-4 rounded hover:bg-slate-600">Feature5</div>
+    <div className="bg-slate-500 p-4 rounded hover:bg-slate-600">Feature6</div>
   </div>
   </div>
   
