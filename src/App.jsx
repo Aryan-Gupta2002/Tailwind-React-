0572b398b1ddac8f59097f3682870a6d308cf5c0
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-11 20:48:30 +0530

    Spacing using Tailwind in React

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (<div>
+    <div className="bg-blue-200 my-3">
+    <p className="text-xl font-semibold text-center text-rose-800">Hello World</p>
+    </div>
+  </div>)
+}
+// p-1 -> 0.25rem. p-2 -> 0.5rem
+export default App
