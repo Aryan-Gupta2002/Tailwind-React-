6663d41d172863a397b577c8ad440324d860fcd3
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-11 21:35:57 +0530

    Elements Resizing

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App() {
       <span>Contact</span>
     </div>
   </div>
-  <div className="grid grid-cols-2">
-    <div className="bg-slate-500 p-4 rounded-xl">Feature1</div>
-    <div>Feature2</div>
-    <div>Feature3</div>
-    <div>Feature4</div>
-    <div>Feature5</div>
-    <div>Feature6</div>
+  <div className="grid sm:grid-cols-2 md:grid-cols-3 text-white p-6 gap-6 text-center font-semibold sm:text-sm ">
+    <div className="bg-slate-500 p-4 rounded">Feature1</div>
+    <div className="bg-slate-500 p-4 rounded">Feature2</div>
+    <div className="bg-slate-500 p-4 rounded">Feature3</div>
+    <div className="bg-slate-500 p-4 rounded">Feature4</div>
+    <div className="bg-slate-500 p-4 rounded">Feature5</div>
+    <div className="bg-slate-500 p-4 rounded">Feature6</div>
   </div>
   </div>
   
@@ -23,4 +23,6 @@ function App() {
 }
 // p-1 -> 0.25rem. p-2 -> 0.5rem
 // flex-col ->flex-direction:column
+// sm:grid-cols-2, means anything bigger than a sm(small screen)
+// will have 2 columns, lesser than sm will have 1 column
 export default App
