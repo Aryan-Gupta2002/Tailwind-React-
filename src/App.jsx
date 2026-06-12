d280889dc0499b5fb4f5aff5822e364f4d4ce4f0
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-12 12:01:18 +0530

    Mobile and Desktop Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+
 function App() {
+  const[open,setOpen]=useState(false);
   return (
   <div>
     {/* Navbar */}
@@ -10,14 +13,14 @@ function App() {
       <span>About</span>
       <span>Contact</span>
     </div>
-    <button>☰</button>
-    {/* Mobile Nav */}
-    <div className="flex gap-2">
+    <button className="text-xl cursor-pointer sm:hidden" onClick={()=>setOpen(!open)}>☰</button>
+  </div>
+  {/* Mobile Nav */}
+    {open &&(<div className="flex flex-col items-center bg-slate-900 text-white gap-2 p-4 sm:hidden">
       <span>Home</span>
       <span>About</span>
       <span>Contact</span>
-    </div>
-  </div>
+    </div>)}
   <div className="grid sm:grid-cols-2 md:grid-cols-3 text-white p-6 gap-6 text-center font-semibold sm:text-sm ">
     <div className="bg-slate-500 p-4 rounded hover:bg-slate-600 hover:scale-105 transition-all duration-500">Feature1</div>
     <div className="bg-slate-500 p-4 rounded hover:bg-slate-600 hover:scale-105">Feature2</div>
